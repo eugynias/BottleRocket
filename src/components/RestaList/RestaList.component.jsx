b53441f611eca60004e3a37fc90095ae509abef4
unknown <eugynias>
2020-07-08 21:25:00 -0400

    Fix bug for fetching data from the API

--- src/components/RestaList/RestaList.component.jsx
+++ src/components/RestaList/RestaList.component.jsx
@@ -5,21 +5,6 @@ import RestaItem from '../../components/RestaItem/RestaItem.component';
 
 const RestaList= ({ restaurants }) => {
 
-  const [list,setList]= useState({restaurants})
-  //console.log(restaurants)
-
-  //Accedo a restaurants
-  const array=(Object.keys(list) );
-
-  
-  console.log(array)
-  
-  // const array1=(Object.keys(array) );
-  // console.log(array1)
-
-  //console.log(Object.hasOwnProperty(array))
-
-
   return (
     <div>
       {/* {restaurants.map(( restaItem ) => (  
