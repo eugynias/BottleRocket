18755bbe362b565b3691e44ae78ff6646279837a
unknown <eugynias>
2020-07-09 18:38:48 -0400

    Resolving problems after checkout branch

--- src/views/RestaView/RestaView.component.jsx
+++ src/views/RestaView/RestaView.component.jsx
@@ -2,20 +2,27 @@ import React, { useState, useEffect }from 'react';
 import HeaderLunch from '../../components/HeaderLunch/HeaderLuch.component';
 import RestaList from '../../components/RestaList/RestaList.component';
 
+import * as Style from './RestaView.style';
+
 
 function  RestaView() {
 
-  const [restaurants, setRestaurants] = useState([ ]);
+  const [restaurants, setRestaurants] = useState([]);
 
   const API_URL="http://sandbox.bottlerocketapps.com/BR_iOS_CodingExam_2015_Server/restaurants.json";
 
 
   const fetchListData = async () => {
-      const response = await fetch(API_URL);
-      const data = await response.json();
-      setRestaurants(data);
-      console.log(data)
-      return data
+      fetch(API_URL).then(
+        function (response){
+          return response.json();
+        }
+      ).then(
+        function(data){
+          console.log(data.restaurants)
+          setRestaurants(data.restaurants)
+        }
+      )
     };
 
 
@@ -25,7 +32,10 @@ function  RestaView() {
 
   return (
     <div>
-
+        <HeaderLunch />
+        <Style.RestImage>
+            <RestaList restaurants={restaurants} />
+        </Style.RestImage>
     </div>
   );
 
