23c1840802fb20dbe6b973c0500d159815e81c81
unknown <eugynias>
2020-07-09 00:30:46 -0400

    Receiving props from restaitem for locationitem

--- src/views/LocaView/LocaView.component.jsx
+++ src/views/LocaView/LocaView.component.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 import { useHistory } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
+import LocationItem from '../../components/LocationItem/LocationItem.component';
 
 
-function LocaView() {
-const { restaItem } = useParams();
 
-console.log('Identificar', restaItem)
+function LocaView({ restaItem }) {
 
 return (
     <>
-        <p> {restaItem}</p>
+        <LocationItem  name ={restaItem.name} lng={restaItem.location.lng} lat={restaItem.location.lat} address={restaItem.location.lat}/>
     </>
 );
 }
 
 export default LocaView;
+
