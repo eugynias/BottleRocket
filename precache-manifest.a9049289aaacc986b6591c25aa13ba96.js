788e37e3fe8b620acb3501060babce13be03b7b6
unknown <eugynias>
2020-07-10 00:38:47 -0400

    Updates

---
+++ precache-manifest.a9049289aaacc986b6591c25aa13ba96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7218e91ebb6677cba6efce1ff6f22a16",
+    "url": "/BottleRocket/index.html"
+  },
+  {
+    "revision": "d4c1a0fc9619f1509bc7",
+    "url": "/BottleRocket/static/js/2.7d22c5e3.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/BottleRocket/static/js/2.7d22c5e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13770c131e1e411cf31",
+    "url": "/BottleRocket/static/js/main.932f5618.chunk.js"
+  },
+  {
+    "revision": "7ce8e9d196c25a069f8d",
+    "url": "/BottleRocket/static/js/runtime-main.18954875.js"
+  },
+  {
+    "revision": "e11f972ccfd8763635a949ff91f3f14c",
+    "url": "/BottleRocket/static/media/Welcome.e11f972c.png"
+  }
+]);
